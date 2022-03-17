61ce90ea27f2f3e6b7f40d99901046d8ebf77809
Ivan <uvanes21@yandex.ru>
2022-03-17 09:13:13 +0300

    Первый коммит

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,9 @@
+package Main;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("Иван"));
+    }
+}
